21a431562c0268437cfa0886f52e22c4bea59669
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-05-29 19:15:42 +0800

    update copyright info in cli.py

--- file_io_cli_tddschn/cli.py
+++ file_io_cli_tddschn/cli.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2018 Niklas Rosenstein
+# Copyright (c) 2023 Teddy Xinyuan Chen
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
